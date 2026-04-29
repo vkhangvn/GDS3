5fa1451792c084bafbfa720a4f0d209e384926be
vkhangvn <vkhangvn@gmail.com>
2026-04-29 16:20:19 +0700

    a

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,13 +7,10 @@ public class Player : MonoBehaviour
     public int score;
     private bool didCollideThisFrame;
 
-    private Rigidbody rb;
     private Rigidbody2D rb2D;
 
-    private HingeJoint hj;
     private HingeJoint2D hj2D;
 
-    private JointMotor motor;
     private JointMotor2D motor2D;
 
 
@@ -21,56 +18,40 @@ public class Player : MonoBehaviour
     void Start()
     {
         //Getting my rigidbody & hingejoints
-        rb = GetComponent<Rigidbody>();
         rb2D = GetComponent<Rigidbody2D>();
-        hj = GetComponent<HingeJoint>();
         hj2D = GetComponent<HingeJoint2D>();
 
-
-        //Checking if I'm in 3D or 2D, then learning my motor
-        if (rb!=null){
-            motor = hj.motor;
-        } else {
-            motor2D = hj2D.motor;
-        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //if 3D
-       if (rb!=null){
-            //when getting Space down
-        if (Input.GetKey(KeyCode.Space)){
-            Debug.Log("pressing space");
-            //Adding motorspeed to my motor
-            motor.targetVelocity = 500f;
-        } else {
-            motor.targetVelocity = -500f;
-        }
-        //MAKE SURE YOU FEED MOTOR DATA INTO THE HINGEJOINT'S MOTOR ("HingeJoint.motor")
-        hj.motor = motor;
-        }
-       
-       /* if (rb2D != null){
-            if (Input.GetKey(KeyCode.Space)){
-            Debug.Log("pressing space");
-            motor2D.motorSpeed = 500f;
-        } else {
-            motor2D.motorSpeed = -500f;
+
+        if (rb2D != null)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("pressing space");
+                motor2D.motorSpeed = 500f;
+            }
+            else
+            {
+                motor2D.motorSpeed = -500f;
+            }
+
+            hj2D.motor = motor2D;
         }
 
-        hj2D.motor = motor2D;
-        }
-       */
     }
     //collision code
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         //if you did collide with this, get out
         if (didCollideThisFrame) return;
 
         //checking that the tag is "edible"
-        if (other.gameObject.CompareTag("Edible")){
+        if (other.gameObject.CompareTag("Edible"))
+        {
             //Up score, destroy other
             Debug.Log("This is edible!");
             score++;
@@ -79,15 +60,6 @@ public class Player : MonoBehaviour
         //now I DID collide with this
         didCollideThisFrame = true;
     }
-
-    private void OnCollisionEnter(Collision other) {
-        if (didCollideThisFrame) return;
-
-        if (other.gameObject.CompareTag("Edible")){
-            score++;
-            Destroy(other.gameObject);
-        }
-
-        didCollideThisFrame = true;
-    }
 }
+
+ 
--- Assets/Slime.cs
+++ Assets/Slime.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    public GameObject waterPrefab;
+    public bool haveWater1;
+    public int waterNumber;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        haveWater1 = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (waterNumber > 400)
+        {
+            haveWater1 = false;
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (haveWater1)
+            { 
+                GameObject newWater = Object.Instantiate(waterPrefab, transform.position, Quaternion.identity);
+                waterNumber++;
+            }
 
+        }
     }
 
-    private void OnCollisionEnter2D(Collider2D other)
+    /*private void OnCollisionEnter2D(Collider2D other)
     {
         if (other.CompareTag("Wall"))
         {
             transform.localScale = new Vector3(2f, 0.1f, transform.localScale.z);
         }
-    }
+    } */
 }
